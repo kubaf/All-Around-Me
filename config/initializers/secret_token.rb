bb496c586169cadd0a23c42cf3cd34903d5041f4
Kuba Fietkiewicz <kuba.fietkiewicz@gmail.com>
2012-02-04 19:50:51 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AllAroundMe::Application.config.secret_token = 'c75be526250eb06e69bd75c0c18da9729b3875e851deff83cb5164786e11dcecd29e78964cddb7b2093d1198e246b8003b639bae74545798349899572dbbb78b'
