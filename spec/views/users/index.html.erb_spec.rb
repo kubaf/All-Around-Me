bf8ff77b05a8c1fad42c3813a6ab22261ee34e44
Kuba Fietkiewicz <kuba.fietkiewicz@gmail.com>
2012-03-15 20:14:00 -0700

    updated user class

---
+++ spec/views/users/index.html.erb_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe "users/index.html.erb" do
+  before(:each) do
+    assign(:users, [
+      stub_model(User,
+        :first_name => "First Name",
+        :last_name => "Last Name",
+        :email => "Email",
+        :admin => 1
+      ),
+      stub_model(User,
+        :first_name => "First Name",
+        :last_name => "Last Name",
+        :email => "Email",
+        :admin => 1
+      )
+    ])
+  end
+
+  it "renders a list of users" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "First Name".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Last Name".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Email".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => 1.to_s, :count => 2
+  end
+end
