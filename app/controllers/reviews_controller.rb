c2339d5d85004e20736a5be16ffe692bc71997e9
Kuba Fietkiewicz <kuba.fietkiewicz@gmail.com>
2012-07-26 07:12:43 -0700

    Added reviews controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,20 @@
+class ReviewsController < ApplicationController
+  before_filter :signed_in_user
+  
+  def create
+    @review = current_user.reviews.build(params[:review])
+    if @review.save
+      format.html { redirect_to dashboard_path, flash: {success: 'Review created!'}}
+    else
+      format.html { render action: "new" }
+  end
+  
+  def new
+    @review = Review.new
+  end
+  
+  def edit
+  end
+  
+  
+end
