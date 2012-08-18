d738f7ef1ed572cc28f7c93e0bf3235fb26a752e
Kuba Fietkiewicz <kuba.fietkiewicz@gmail.com>
2012-08-18 13:03:57 -0700

    Removed actions form reviewreviewers controller

--- app/controllers/review_reviewers_controller.rb
+++ app/controllers/review_reviewers_controller.rb
@@ -2,14 +2,5 @@ class ReviewReviewersController < ApplicationController
   before_filter :signed_in_user
   before_filter :correct_review, only: [:show, :edit, :update]
   
-  def show
-    @review = Review.find(params[:id])
-    @invited_review_reviewer = ReviewReviewer.new
-    @invited_reviewer = Person.new
-  end
-  
-  def create
-    pp env
-  end
   
 end
